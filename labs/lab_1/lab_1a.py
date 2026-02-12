2de856ab88a89f0c2e0a000bb7a136974a2f82e2
juha1ee <167262024+juha1ee@users.noreply.github.com>
2026-02-11 20:08:10 -0500

    practice edit to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Juha Lee" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, I'm Juha and I'm a junior from New Jersey.")
 
 if __name__ == "__main__":
     main()
